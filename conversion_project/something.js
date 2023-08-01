ec0a5a6c81b39e0f80060cce4afe79ffb1545dc3
Chloe Brown <clbrown1@email.neit.edu>
2023-08-01 12:54:10 -0400

    week 2 player class javascript assignment

---
+++ conversion_project/something.js
@@ -0,0 +1,64 @@
+
+var try1 = 46;
+
+/*
+if
+
+*/
+
+
+console.log(try1);
+
+/*
+console.log
+toExponential(try1)
+
+set a variable equal to the ____ equilvalent
+
+
+*/
+
+//var num2 = Number(try1) / 2;
+
+//console.log("variable num2 is " + num2);
+
+var answer3 = "Y";
+
+var some34 = 2;
+
+if (some34 < 25)
+{
+    
+    
+
+    var num2 = try1 / 2;
+
+    console.log("variable number 2 is set to: " + num2);
+    
+    var num3 = num2 / 2;
+
+    console.log("num3 is set to: " + num3);
+
+    var num4 = num3 / 2;
+
+    console.log("num4 is set to: " + num4);
+
+    var num5 = num4 / 2;
+
+    console.log("Num5 is set to: " + num5);
+
+    some34 += 2;
+
+    //let answer3 = prompt("Do you want to continue?: ");
+
+    //hopefully it will keep going until it can not do the math anymore
+    
+
+    //let answer3 = prompt("Do you want to continue?: ");
+    //console.log(answer3);
+
+
+}
+
+console.log("Have a goooood dayyy!!!!");
+
---
+++ pong (1)/pong/js/Player.js
@@ -0,0 +1,13 @@
+//create a class called player
+
+class Player{
+
+    constructor()
+    {
+        this.name1 = `name`;
+        this.score = 0;
+        this.highScore = 10;
+        this.pad = 4;
+    }
+
+}
